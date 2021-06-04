027b134b02ecc55a7b72cc11f3fd88bc3018f8b4
Prasad <prasad.yoaspire@gmail.com>
2021-06-04 16:28:39 +0530

    project deleted.

--- springbootmvc/src/main/java/com/cg/springbootmvc/repository/TraineeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cg.springbootmvc.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.cg.springbootmvc.entity.Trainee;
-
-public interface TraineeRepository  extends JpaRepository<Trainee,Integer>{
-
-}
